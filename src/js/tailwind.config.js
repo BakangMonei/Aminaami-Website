795cf47d605517284191d3ca3c0625fe0decb0b0
BakangMonei <bakangmonei2@gmail.com>
2024-01-19 16:48:19 +0200

    Added dark mocd thing

--- src/js/tailwind.config.js
+++ src/js/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
+    darkMode: ['class', '[data-mode="dark"]'],
     theme: {
       extend: {},
     },
